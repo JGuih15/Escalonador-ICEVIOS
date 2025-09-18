ab94379e6cfd6751e25c017fad31cbccda45027c
Luís Gabriel Fonseca Prado <gabfonpr@gmail.com>
2025-09-17 22:53:42 -0300

    Add files via upload

--- escalonador/src/scheduler.java
+++ escalonador/src/scheduler.java
@@ -1,87 +1,108 @@
-import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.Scanner;
+public class scheduler {
+    ListaDupla AltaPrioridade = new ListaDupla();
+    ListaDupla MediaPrioridade = new ListaDupla();
+    ListaDupla baixaPrioridade = new ListaDupla();
+    ListaDupla bloqueados = new ListaDupla();
+    ListaCircular listaExecucao = new ListaCircular();
 
-public class scheduler{
-    private scheduler prioridade;
-    public Listacircular altaPrioridade = new Listacircular();
-    public Listacircular mediaPrioridade = new Listacircular();
-    public Listacircular baixaPrioridade = new Listacircular();
-    public Listacircular todos = new Listacircular();
+    public int ciclos;
+    private int contadorAlta = 0;
 
-    public scheduler(){
-        this.prioridade=new scheduler();
-    }
-
-    public scheduler getAltaPrioridade() {return prioridade;}
-    public void setAltaPrioridade(scheduler prioridade) {this.prioridade = prioridade;}
-
-    public scheduler getMediaPrioridade() {return prioridade;}
-    public void setMediaPrioridade(scheduler prioridade) {this.prioridade = prioridade;}
-
-    public scheduler getBaixaPrioridade() {return prioridade;}
-    public void setBaixaPrioridade(scheduler prioridade) {this.prioridade = prioridade;}
-
-    //analisar os dados e separar por meio da casse prioridade.
-    public void leitura(String arquivo) throws IOException{
-        Processos[] processos= LeitordeDados.leituraTXT(arquivo);
-        for(Processos p: processos){
-           if(p==null) continue;
-          else if (p==1){
-              altaPrioridade.addProcessoLC(p);
-              todos.addProcessoLC(p);
-           }
-           else if (p==2){
-               mediaPrioridade.addProcessoLC(p);
-               todos.addProcessoLC();
-           }
-           else if (p==3){
-               baixaPrioridade.addProcessoLC(p);
-               todos.addProcessoLC(p);
-           }else return;
+    public void adicionar_Processo(Processos processo) {
+        switch (processo.getPrioridade()) {
+            case 1:
+                AltaPrioridade.adicionarNoFinal(processo);
+                break;
+            case 2:
+                MediaPrioridade.adicionarNoFinal(processo);
+                break;
+            case 3:
+                baixaPrioridade.adicionarNoFinal(processo);
+                break;
         }
     }
-    public void executarCiclo(){
-        while(true){
-            int contadorAlta = 0;
-            if(contadorAlta<5){
-                System.out.println("processo de prioridade 1:");
-                System.out.println(altaPrioridade.toString());
-                altaPrioridade.proximoElemento();
-                contadorAlta++;
-            }else {
-                System.out.println("processo de prioridade 2:");
-                System.out.println(mediaPrioridade.toString());
-                System.out.println("processo de prioridade 3:");
-                System.out.println(baixaPrioridade.toString());
 
-                contadorAlta = 0;
+    private void desbloquearProcesso() {
+        if (bloqueados.getTamanho() > 0) {
+            Processos processoDesbloqueado = bloqueados.removerDoInicio();
+            if (processoDesbloqueado.getPrioridade() == 1) {
+                AltaPrioridade.adicionarNoFinal(processoDesbloqueado);
+            } else if (processoDesbloqueado.getPrioridade() == 2) {
+                MediaPrioridade.adicionarNoFinal(processoDesbloqueado);
+            } else if (processoDesbloqueado.getPrioridade() == 3) {
+                baixaPrioridade.adicionarNoFinal(processoDesbloqueado);
             }
-            mediaPrioridade.proximoElemento();
-            baixaPrioridade.proximoElemento();
         }
     }
 
-
-    public void executar(){
-        System.out.println("alta prioridade: "+prioridade.getAltaPrioridade());
-        System.out.println("meida prioridade: "+prioridade.getMediaPrioridade());
-        System.out.println("Baixa prioridade: "+prioridade.getBaixaPrioridade());
-    }
-
-    private void executarFila(String nomeFila, gerenciamento fila) {
-        System.out.println("---- " + nomeFila + " ----");
-        while (!fila.isEmpty()) {
-            Processos p = fila.removerUltima();
-            System.out.println("Executando: " + p.getNome() +
-                    " | Prioridade: " + p.getPrioridade() +
-                    " | Ciclos: " + p.getCiclos());
-
+    private void moverParaExecucao() {
+        if (contadorAlta >= 5) {
+            if (MediaPrioridade.getTamanho() > 0) {
+                listaExecucao.adicionarFim(MediaPrioridade.removerDoInicio());
+            } else if (baixaPrioridade.getTamanho() > 0) {
+                listaExecucao.adicionarFim(baixaPrioridade.removerDoInicio());
+            }
+            contadorAlta = 0;
+        } else {
+            if (AltaPrioridade.getTamanho() > 0) {
+                listaExecucao.adicionarFim(AltaPrioridade.removerDoInicio());
+                contadorAlta++;
+            } else if (MediaPrioridade.getTamanho() > 0) {
+                listaExecucao.adicionarFim(MediaPrioridade.removerDoInicio());
+            } else if (baixaPrioridade.getTamanho() > 0) {
+                listaExecucao.adicionarFim(baixaPrioridade.removerDoInicio());
+            }
         }
     }
 
+    private void executarAtual() {
+        if (listaExecucao.estaVazia()) return;
+        Processos atual = listaExecucao.getAtual();
+        if (atual.getRecursos() > 0 && atual.isBloqueado() == false) {
+            atual.setBloqueado(true);
+            bloqueados.adicionarNoFinal(atual);
+            listaExecucao.removerAtual();
+            return;
+        }
+        atual.setCiclos(atual.getCiclos() - 1);
+        if (atual.getCiclos() <= 0) {
+            listaExecucao.removerAtual();
+        } else {
+            listaExecucao.avancar();
+        }
+    }
 
+    private void imprimirEstado() {
+        System.out.println("--- ESTADO DO SISTEMA ---");
+        System.out.println("Alta prioridade:");
+        AltaPrioridade.imprimirLista();
+        System.out.println("Media prioridade:");
+        MediaPrioridade.imprimirLista();
+        System.out.println("Baixa prioridade:");
+        baixaPrioridade.imprimirLista();
+        System.out.println("Bloqueados:");
+        bloqueados.imprimirLista();
+        System.out.println("Execução:");
+        listaExecucao.imprimirLista();
+        System.out.println("Ciclos executados: " + ciclos);
+    }
 
+    public boolean temProcesso() {
+        return !AltaPrioridade.estaVazia() || 
+               !MediaPrioridade.estaVazia() || 
+               !baixaPrioridade.estaVazia() || 
+               !bloqueados.estaVazia() || 
+               !listaExecucao.estaVazia();
+    }
 
-
+    public void execucaoCompleta() {
+        while (temProcesso()) {
+            ciclos++;
+            desbloquearProcesso();
+            moverParaExecucao();
+            if (!listaExecucao.estaVazia()) executarAtual();
+            imprimirEstado();
+        }
+        System.out.println("Execução finalizada. Total de ciclos: " + ciclos);
+    }
 }
